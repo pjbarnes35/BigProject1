31c7aed11d8f4dee3ad4a5b8949d06c606421629
Pj Barnes <pjbarnes2003@gmail.com>
2024-03-08 11:06:48 -0600

    All questions have been added

--- Database/create2.py
+++ Database/create2.py
@@ -2,9 +2,9 @@ import sqlite3
 conn = sqlite3.connect("QnA2.db")
 cr = conn.cursor()
 
-#creates new tables
+# #creates new tables
 # cr.execute("""
-#     CREATE TABLE IF NOT EXISTS Advanced_Analytics (
+#     CREATE TABLE IF NOT EXISTS Buss_Strat (
 #            id INTEGER PRIMARY KEY,
 #            Question TEXT,
 #            Answer TEXT)
